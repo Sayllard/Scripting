a88530a52411f4308bb3b92e2d3094f63188a86d
Sayllard <135047667+Sayllard@users.noreply.github.com>
2023-11-07 18:26:34 +0300

    Add files via upload
    Скрипт на C# Hello World

---
+++ Hello_world.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello_world : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Выводит фразу "Hello World"
+        print("Hello World");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
